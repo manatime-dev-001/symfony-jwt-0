d0a0ea6dea892536fa25946aa6cae0791b5e6245
harivansh <harivansh@manatime.com>
2023-01-31 12:24:51 +0400

    feat: implement JwtCookieAuthenticator

---
+++ src/Security/JwtCookieAuthenticator.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use App\Service\JwtService;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
+
+class JwtCookieAuthenticator extends AbstractAuthenticator
+{
+    public function __construct(
+        private readonly JwtService $jwtService,
+        private readonly EntityManagerInterface $entityManager,
+    ) {
+        //
+    }
+
+    public function supports(Request $request): ?bool
+    {
+        return $request->cookies->has('auth-jwt');
+    }
+
+    public function authenticate(Request $request): Passport
+    {
+        $token = $request->cookies->get('auth-jwt');
+
+        try {
+            $claims = $this->jwtService->decodeToken($token);
+        } catch (Exception $exception) {
+            throw new CustomUserMessageAuthenticationException($exception->getMessage());
+        }
+
+        return new SelfValidatingPassport(new UserBadge($claims['user_id'], function (int $userId) {
+            return $this->entityManager->getRepository(User::class)->find($userId);
+        }));
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        return null;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
+        return new Response(status: Response::HTTP_UNAUTHORIZED);
+    }
+
+//    public function start(Request $request, AuthenticationException $authException = null): Response
+//    {
+//        /*
+//         * If you would like this class to control what happens when an anonymous user accesses a
+//         * protected page (e.g. redirect to /login), uncomment this method and make this class
+//         * implement Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface.
+//         *
+//         * For more details, see https://symfony.com/doc/current/security/experimental_authenticators.html#configuring-the-authentication-entry-point
+//         */
+//    }
+}
